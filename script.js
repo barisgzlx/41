e5c3fcdb7473e8a2df47e1baf11f01a332425af1
barisgzlx <baropqsawt@gmail.com>
2026-01-30 05:37:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 let users = JSON.parse(localStorage.getItem("users")) || [];
-let posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+/* KAYIT GÖSTER */
+function showRegister(){
+  document.getElementById("registerBox").style.display = "block";
+}
+
+/* KAYIT OL */
+function register(){
+  let u = regUser.value;
+  let m = regMail.value;
+  let p = regPass.value;
+
+  if(!u || !m || !p){
+    alert("Tüm alanları doldur");
+    return;
+  }
+
+  users.push({user:u, mail:m, pass:p});
+  localStorage.setItem("users", JSON.stringify(users));
+
+  alert("Kayıt başarılı, giriş yapabilirsin");
+  document.getElementById("registerBox").style.display = "none";
+}
 
 /* MÜŞTERİ GİRİŞ */
 function login(){
-  let u = document.getElementById("logUser").value;
-  let p = document.getElementById("logPass").value;
+  let u = logUser.value;
+  let p = logPass.value;
 
   let ok = users.find(x => x.user === u && x.pass === p);
 
   if(ok){
-    alert("Müşteri girişi başarılı");
+    alert("Giriş başarılı 🎉");
   }else{
-    alert("Kullanıcı adı veya şifre yanlış");
+    alert("Hatalı kullanıcı adı veya şifre");
   }
 }
