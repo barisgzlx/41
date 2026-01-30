d8b652883cfb191f66947a68a07c8492165a2e07
barisgzlx <baropqsawt@gmail.com>
2026-01-30 05:35:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,39 +10,7 @@ function login(){
 
   if(ok){
     alert("Müşteri girişi başarılı");
-    // müşteri paneli sonra eklenecek
   }else{
     alert("Kullanıcı adı veya şifre yanlış");
   }
-}
-
-/* ADMIN GİRİŞ */
-function adminLogin(){
-  let u = document.getElementById("aUser").value;
-  let p = document.getElementById("aPass").value;
-
-  if(u === "barisguzel" && p === "baro1453"){
-    document.getElementById("adminLoginBox").style.display = "none";
-    document.getElementById("adminPanel").style.display = "block";
-    loadPosts();
-  }else{
-    alert("Admin bilgileri hatalı");
-  }
-}
-
-/* ADMIN PAYLAŞIMLARI YÜKLE */
-function loadPosts(){
-  let list = document.getElementById("postList");
-  let html = "";
-
-  posts.forEach(p=>{
-    html += `
-      <div class="post">
-        <b>Kullanıcı:</b> ${p.user}<br>
-        <b>Söz:</b> ${p.text}
-      </div>
-    `;
-  });
-
-  list.innerHTML = html || "Henüz müşteri paylaşımı yok";
 }
